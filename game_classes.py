0e51bb2a6658c7a7efcf19c4f8998dfdb543d0c9
jasper postema <jasperpostema@hotmail.nl>
2021-10-06 01:22:40 +0200

    Made the game class, added some comments

--- game_classes.py
+++ game_classes.py
@@ -1,17 +1,49 @@
 #Tips from: https://www.youtube.com/watch?v=t8YkjDH86Y4&t=1s
+
 import random as rand
 
-#Need to make class Game
-#class Game(object,players_nr):
-#    def __init__(self):
-#        Plist = []
-#        for nr in range(players_nr):
-#            Plist.append(Player(nr))
+#The game class contains the game, it sets up the players and keeps track of the turns and who is playing and such.
+class Game(object):
+    def __init__(self,players_nr):
+        self.garbage = Garbage()#This is easy, just empty from the start
+        self.field = Field()#This should need some input, basically the cards we play with
+
+        #Setting up players
+        self.players = {}
+        self.nr_players = players_nr
+        for nr in range(players_nr):
+            self.players[str(nr)] = Player(self,nr)
+
+        self.game_start()
+
+    def game_start(self): #Setting round and selecting starting player, also starting their tur
+        self.round = 1
+        self.first_player = rand.randint(1,self.nr_players)
+        self.p_turn = self.first_player
+        self.start_turn()
+    
+    def start_turn(self): #unfinished, this should basically run the method that allows interaction with the hand
+        print('hi')
+        #if turn has been ended (need to add interactivity for that) maybe in different function
+        #self.end_turn()
+
+    def end_turn(self): #Turn end sequence, updating round number if needed and player turn
+        self.next_player()
+        if self.p_turn == self.first_player:
+            self.next_round()
+        self.start_turn()        
+
+    def next_player(self):
+        self.p_turn = self.p_turn % self.nr_players + 1
+
+    def next_round(self):
+        self.round += 1
+    
 
 
 #The player class keeps track of which hand, deck and discard pile belongs to an individual, maybe add actions to here as well?
 class Player(object):
-    def __init__(self,nr):
+    def __init__(self,game,nr): #Might need the game later on for certain functions
         self.number = nr
         self.deck = Deck()
         self.hand = Hand()
@@ -25,7 +57,7 @@ class Player(object):
         self.in_play.discard_play(self)
 
 
-#The card class should just be a storage of the cards maybe also what happens when you play it?
+#The card class should just be a storage of the cards maybe also what happens when you play it? (Maybe use switch statement for that in newest python version)
 class Card(object):
     def __init__(self,type):
         self.type = type
@@ -53,12 +85,12 @@ class Deck(object):
         self.cards = starting_deck
         self.shuffle()
     
-    def shuffle(self):
+    def shuffle(self): #shuffling the deck, no seeding has been implemented, might do that later
         for i in range(len(self.cards)-1,0,-1):
             r = rand.randint(0,i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
-    def draw(self, player, amount = 1):
+    def draw(self, player, amount = 1): #Drawing a card, will also rebuild the deck if you try to draw when the deck is empty
         for i in range(amount):
             if len(self.cards) != 0:
                 drawn = self.cards.pop()
@@ -70,7 +102,7 @@ class Deck(object):
             else:
                 print("could not draw any more") #Maybe do something else with this later
         
-    def rebuild(self, player):
+    def rebuild(self, player): #Rebuilding the deck, putting the deck into the discard pile taking that discard pile, making it empty in the process, and making that into the deck by shuffling
         player.discard_pile.cards.extend(self.cards)
         self.cards = player.discard_pile.cards
         player.discard_pile.destroy()
@@ -97,13 +129,16 @@ class Hand(object):
     def __init__(self):
         self.cards = []
 
+    #Drawing the 5 cards to fill the hand
     def fill_hand(self,player):
         player.deck.draw(player,5)
     
+    #Throwing the hand away at the end of the turn
     def discard_hand(self,player):
         player.discard_pile.cards.extend(self.cards)
         self.cards = []
 
+    #Refilling your hand after your turn has ended
     def redraw_hand(self,player):
         self.discard_hand(player)
         self.fill_hand(player)
@@ -112,11 +147,12 @@ class Hand(object):
         return [c.show() for c in self.cards]
 
 
-#In play class represents the cards that are currently in play (can't be shuffled in dack) should be able to discard into discard pile
+#In play class represents the cards that are currently in play (can't be shuffled in deck for the time) should be able to discard into discard pile
 class In_play(object):
     def __init__(self):
         self.cards = []
 
+    #Putting the in play cards into the discard pile
     def discard_play(self,player):
         player.discard_pile.cards.extend(self.cards)
         self.cards = []
@@ -125,7 +161,14 @@ class In_play(object):
         return [c.show() for c in self.cards]
 
 
-P1 = Player(1)
+G = Game(4)
+print(G.p_turn,G.round)
+for _ in range(10):
+    G.end_turn()
+    print(G.p_turn,G.round)
+
+'''
+P1 = G.players['1']
 print(P1.hand)
 print(P1.deck.show())
 print(P1.hand.show())
@@ -138,9 +181,9 @@ P1.hand.redraw_hand(P1)
 print(P1.deck.show())
 print(P1.hand.show())
 print(P1.discard_pile.show())
+'''
 
-
-
+#The cards that have been destroyed, should just be a collection that can do nothing
 class Garbage(object):
     def __init__(self):
         self.cards = []
@@ -148,14 +191,18 @@ class Garbage(object):
     def show(self):
         return [c.show() for c in self.cards]
 
+
+#Collection of piles of cards that can be bought, can be selected from and update the amount of cards in those piles
 class Field(object):
     def __init__(self):
         self.piles = []
         #Fill the field with card piles that are chosen and the default ones
 
+
+#Just a list of cards in a pile on the field should just be an amount and card object
 class Field_pile(object):
-    def __init__(self):
+    def __init__(self,type):
         self.amount = 0
-        self.type = ''
+        self.card = Card(type)
 
 
