2c041c0806a2cb593510b4cfef433874f6811570
jasper postema <jasperpostema@hotmail.nl>
2022-05-14 21:37:36 +0200

    Card position in hand can now be changed

--- Pygame_try.py
+++ Pygame_try.py
@@ -1,58 +1,21 @@
+import this
+from turtle import bgcolor
 import pygame
 import game_classes as gc
 import numpy as np
 
-def angle_for_card_arch(tot_cards,card_id,outer_ang):
-    return 2*outer_ang/(tot_cards+1)*(card_id+1)-outer_ang
-
-def arch_position(cards, scaled_W = 200, N = 7, outer_angle = 45):
-    tot_cards = len(cards)  #Maybe make part of hand object
-    arch_N = sum([np.cos(np.pi*angle_for_card_arch(tot_cards,j,outer_angle)/180) for j in range(N)])  
-    im = cards[0].image.convert_alpha()
-
-    W = scaled_W
-    H = W*im.get_height()/im.get_width()  #Probably make these fixed and globally accesible
-
-    first_angle = angle_for_card_arch(tot_cards,0,outer_angle)
-    N_W_outer = W*np.cos(np.pi*first_angle/180)+H*np.sin(abs(np.pi*first_angle/180))
-    R = abs((arch_N*W/2 - N_W_outer/2)/np.sin(np.pi*first_angle/180))
-    Scale_height = (R*np.cos(np.pi*first_angle/180)-(H*np.cos(np.pi*first_angle/180)+W*np.sin(abs(np.pi*first_angle/180)))/2)
-
-    for i,card_sprite in enumerate(G.players[this_player].hand.cards):
-        im = card_sprite.image.convert_alpha()
-
-        angle = angle_for_card_arch(tot_cards,i,outer_angle)
-        N_W = W*np.cos(np.pi*angle/180)+H*np.sin(abs(np.pi*angle/180))
-        N_D = H*np.cos(np.pi*angle/180)+W*np.sin(abs(np.pi*angle/180))
-        DW = R*np.sin(np.pi*angle/180)-N_W/2
-        DH = R*np.cos(np.pi*angle/180)+N_D/2-Scale_height
-
-        screen.blit(pygame.transform.rotate(pygame.transform.scale(im,(W,H)),-angle),(WIDTH/2+DW,HEIGHT-DH))
-
-def line_position(cards, scaled_W = 200, N = 5):
-    tot_cards = len(cards)  #Maybe make part of hand object
-    im = cards[0].image.convert_alpha()
-
-    W = scaled_W
-    H = W*im.get_height()/im.get_width()  #Probably make these fixed and globally accesible
-
-    for i,card_sprite in enumerate(G.players[this_player].hand.cards):
-        im = card_sprite.image.convert_alpha()
-
-        screen.blit(pygame.transform.scale(im,(W,H)),(W*(N-1)/(tot_cards+1)*(i+1)+WIDTH/2-W*N/2,HEIGHT-H)) #move somewhere else
-
 #Probably should make all of this a class as well that is initialized when we go from menu screen to game
-G = gc.Game(4)
-pygame.init()
-pygame.display.set_caption("Dominion")
 WIDTH = 1550
 HEIGHT = 801
 FPS = 30
+BG_Colour = (255, 10, 255)
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 clock = pygame.time.Clock()
-screen.fill((255, 255, 255)) #This should be the background
 
+G = gc.Game(4,Screen = screen)
+pygame.init()
+pygame.display.set_caption("Dominion")
 
 #copper = gc.Card("Copper")
 
@@ -66,20 +29,33 @@ this_player = 1
 G.players[this_player].deck.draw(G.players[this_player],5)
 
 running = True
+I = 0
 while running:
+    screen.fill(BG_Colour)
 
     clock.tick(FPS)
+    mouse = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
-
-    W = 150
-
-    line_position(G.players[this_player].hand.cards, scaled_W = W)
-    arch_position(G.players[this_player].hand.cards, scaled_W = W)
         
-        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #if "clicked on card":
+                #Play that card
+            #else:
+            G.players[this_player].hand.swap_hand_position()
+
+            #some buttons in between as well
+
+
+    G.players[this_player].hand.set_hand_position(screen)
+    print([c.rect.center for c in G.players[this_player].hand.cards])
+
+           
     pygame.display.flip()
-
 pygame.quit()
+
+#TO DO:
+#ALLOW TO CLICK ON A CARD
+#ALLOW A CLICKED CARD TO BE PLAYED
--- game_classes.py
+++ game_classes.py
@@ -1,21 +1,22 @@
 #Tips from: https://www.youtube.com/watch?v=t8YkjDH86Y4&t=1s
 
 import random as rand
+import numpy as np
 import pygame
 import os
 
 #The game class contains the game, it sets up the players and keeps track of the turns and who is playing and such.
 class Game(object):
     #Maybe rewrite init so that it creates the player selection menu instead and make this init what happens after
-    def __init__(self,players_nr): #Card set should also be an input
+    def __init__(self,players_nr,**kwargs): #Card set should also be an input
         self.garbage = Garbage()#This is easy, just empty from the start
         self.field = Field()#This should need some input, basically the cards we play with
-
+        self.turn_nr = 1
         #Setting up players
         self.players = {}
         self.nr_players = players_nr
         for nr in range(players_nr):
-            self.players[nr] = Player(self,nr)
+            self.players[nr] = Player(self,nr,**kwargs)
 
         self.game_start()
 
@@ -26,7 +27,7 @@ class Game(object):
         self.start_turn()
     
     def start_turn(self): #unfinished, this should basically run the method that allows interaction with the hand
-        print('hi')
+        print(f'Turn {self.round} for player {self.p_turn}')
         #if turn has been ended (need to add interactivity for that) maybe in different function
         #self.end_turn()
 
@@ -43,14 +44,15 @@ class Game(object):
         self.round += 1
     
 
-
 #The player class keeps track of which hand, deck and discard pile belongs to an individual, maybe add actions to here as well?
 class Player(object):
-    def __init__(self,game,nr): #Might need the game later on for certain functions
+    def __init__(self,game,nr,**kwargs): #Might need the game later on for certain functions
         self.number = nr
         self.deck = Deck()
-        self.hand = Hand()
+        self.hand = Hand("line")
         self.hand.fill_hand(self)
+        screen = kwargs.get("Screen")
+        self.hand.set_hand_position(screen)
         self.discard_pile = Discard_pile()
         self.in_play = In_play()
 
@@ -71,26 +73,32 @@ class Card(pygame.sprite.Sprite):
         self.image = pygame.image.load(self.path)
         self.rect = self.image.get_rect()
 
+    def __str__(self):
+        return f"{self.type}"
+
     def play(self,player):
         print(self.type)
         #Should be something that happens when you play the card
         player.in_play.cards.append(self)
-
-    def show(self):
-        return self.type
 
 
 #the deck class should be a storage of a collection of cards and should be able to shuffle and draw 
 class Deck(object):
     def __init__(self):
         self.build()
+    
+    def __str__(self):
+        card_string = ""
+        for c in self.cards:
+            card_string += f"{c}, "
+        return card_string[:-2]
 
     def build(self): #Could add set keyword for different starting sets
-        start_cards = [Card("Estate"),Card("Copper")]
-        number_cards = [3,7]
+        start_conditions = {"Copper":7,"Estate":3}
         starting_deck = []
-        for i,n in enumerate(start_cards):
-            starting_deck.extend([n]*number_cards[i])
+        for name,amount in start_conditions.items():
+            for i in range(amount):
+                starting_deck.append(Card(name))
         self.cards = starting_deck
         self.shuffle()
     
@@ -117,26 +125,36 @@ class Deck(object):
         player.discard_pile.destroy()
         self.shuffle()
 
-    def show(self):
-        return [c.show() for c in self.cards]
-
 
 #The Discard_pile class should just be a storage of the cards in the discard pile and should be able to destroy itself 
 class Discard_pile(object):
     def __init__(self):
         self.cards = []
+
+    def __str__(self):
+        card_string = ""
+        for c in self.cards:
+            card_string += f"{c}, "
+        return card_string[:-2]
     
     def destroy(self):
-        self.cards = []
-
-    def show(self):
-        return [c.show() for c in self.cards]
+        self.cards = []        
 
 
 #Hand class should be a storage of the cards in hands and should be able to fill and be discarded into discard pile
 class Hand(object):
-    def __init__(self):
+    def __init__(self,position = "line"):#position in which the cards are visualized (line or arch)
         self.cards = []
+        self.position = position
+
+    def __str__(self):
+        card_string = ""
+        for c in self.cards:
+            card_string += f"{c}, "
+        return card_string[:-2]
+
+    def hand_size(self):
+        return len(self.cards)
 
     #Drawing the 5 cards to fill the hand
     def fill_hand(self,player):
@@ -152,22 +170,91 @@ class Hand(object):
         self.discard_hand(player)
         self.fill_hand(player)
 
-    def show(self):
-        return [c.show() for c in self.cards]
+    #3 below need fixing
+    def arch_position(self,screen, scaled_W = 200, N = 7, outer_angle = 45):
+        tot_cards = self.hand_size() 
+        arch_N = sum([np.cos(np.pi*self.angle_for_card_arch(tot_cards,j,outer_angle)/180) for j in range(N)])  
+        im = self.cards[0].image.convert_alpha() #Rather have something else
+
+        W = scaled_W
+        H = W*im.get_height()/im.get_width()  #Probably make these fixed and globally accesible
+
+        first_angle = self.angle_for_card_arch(tot_cards,0,outer_angle)
+        N_W_outer = W*np.cos(np.pi*first_angle/180)+H*np.sin(abs(np.pi*first_angle/180))
+        R = abs((arch_N*W/2 - N_W_outer/2)/np.sin(np.pi*first_angle/180))
+        Scale_height = (R*np.cos(np.pi*first_angle/180)-(H*np.cos(np.pi*first_angle/180)+W*np.sin(abs(np.pi*first_angle/180)))/2)
+        
+        WIDTH = screen.get_width()
+        HEIGHT = screen.get_height()
+
+        for i,card_sprite in enumerate(self.cards):
+            im = card_sprite.image.convert_alpha()
+
+            angle = self.angle_for_card_arch(tot_cards,i,outer_angle)
+            N_W = W*np.cos(np.pi*angle/180)+H*np.sin(abs(np.pi*angle/180))
+            N_D = H*np.cos(np.pi*angle/180)+W*np.sin(abs(np.pi*angle/180))
+            DW = R*np.sin(np.pi*angle/180)-N_W/2
+            DH = R*np.cos(np.pi*angle/180)+N_D/2-Scale_height
+
+            rotated_card = pygame.transform.rotate(pygame.transform.scale(im,(W,H)),-angle)
+
+            screen.blit(rotated_card,(WIDTH/2+DW,HEIGHT-DH))
+
+            card_sprite.rect = rotated_card.get_rect()
+            card_sprite.rect.center = (WIDTH/2+DW,HEIGHT-DH)
+
+    def line_position(self,screen, scaled_W = 200, N = 5):
+        tot_cards = self.hand_size()   
+        im = self.cards[0].image.convert_alpha()
+
+        W = scaled_W
+        H = W*im.get_height()/im.get_width()  
+
+        WIDTH = screen.get_width()
+        HEIGHT = screen.get_height()
+
+        for i,card_sprite in enumerate(self.cards):
+            im = card_sprite.image.convert_alpha()
+
+            scaled_card = pygame.transform.scale(im,(W,H))
+
+            screen.blit(scaled_card,(W*(N-1)/(tot_cards+1)*(i+1)+WIDTH/2-W*N/2,HEIGHT-H)) 
+
+            card_sprite.rect = scaled_card.get_rect()
+            card_sprite.rect.center = (W*(N-1)/(tot_cards+1)*(i+1)+WIDTH/2-W*N/2,HEIGHT-H)
+
+    def angle_for_card_arch(self,tot_cards,card_id,outer_ang): #check if can made less kwargs
+        return 2*outer_ang/(tot_cards+1)*(card_id+1)-outer_ang
+
+    def set_hand_position(self,screen):
+        position = self.position
+        if self.hand_size() != 0:
+            if position == "line":
+                self.line_position(screen)
+            elif position == "arch":
+                self.arch_position(screen)
+            else:
+                raise Exception(f"{position} is not a hand layout") 
+    
+    def swap_hand_position(self):
+        self.position = {"line":"arch","arch":"line"}[self.position]
 
 
 #In play class represents the cards that are currently in play (can't be shuffled in deck for the time) should be able to discard into discard pile
 class In_play(object):
     def __init__(self):
         self.cards = []
+    
+    def __str__(self):
+        card_string = ""
+        for c in self.cards:
+            card_string += f"{c}, "
+        return card_string[:-2]
 
     #Putting the in play cards into the discard pile
     def discard_play(self,player):
         player.discard_pile.cards.extend(self.cards)
-        self.cards = []
-
-    def show(self):
-        return [c.show() for c in self.cards]
+        self.cards = []        
 
 
 #The cards that have been destroyed, should just be a collection that can do nothing
@@ -175,8 +262,8 @@ class Garbage(object):
     def __init__(self):
         self.cards = []
 
-    def show(self):
-        return [c.show() for c in self.cards]
+    def __str__(self):
+        return [c for c in self.cards]
 
 
 #Collection of piles of cards that can be bought, can be selected from and update the amount of cards in those piles
@@ -202,17 +289,17 @@ for _ in range(10):
 
 P1 = G.players[1]
 print(P1.hand)
-print(P1.deck.show())
-print(P1.hand.show())
-print(P1.discard_pile.show())
+print(P1.deck)
+print(P1.hand)
+print(P1.discard_pile)
 P1.hand.redraw_hand(P1)
-print(P1.deck.show())
-print(P1.hand.show())
-print(P1.discard_pile.show())
+print(P1.deck)
+print(P1.hand)
+print(P1.discard_pile)
 P1.hand.redraw_hand(P1)
-print(P1.deck.show())
-print(P1.hand.show())
-print(P1.discard_pile.show())
+print(P1.deck)
+print(P1.hand)
+print(P1.discard_pile)
 '''
 
 
